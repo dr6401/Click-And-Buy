7ad1c0176398f56e58f69abd0a2291ac13701b76
dr6401 <dr6401@student.uni-lj.si>
2026-05-06 23:38:46 +0200

    Added Fund management tab

--- Assets/SCRIPTS/GameConstants.cs
+++ Assets/SCRIPTS/GameConstants.cs
@@ -23,7 +23,7 @@ public static class GameConstants
         public static Color redColor = new Color32(255, 65, 74, 255);
         
         public static Color tokenColor = new Color32(0, 253, 255, 255);
-        public static Color faithColor = new Color32(255, 4+243, 144, 255);
+        public static Color faithColor = new Color32(255, 243, 144, 255);
         
         public static Color commonTierColor = new Color32(46, 219, 19, 255);
         public static Color rareTierColor = new Color32(6, 182, 219, 255);
